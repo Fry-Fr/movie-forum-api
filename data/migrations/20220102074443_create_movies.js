f7b1f79d13e12455b48426660deb0a2cf39c935d
Fry-Fr <ryjlwdg89@gmail.com>
2022-01-02 03:24:51 -0500

    added timestamp

--- data/migrations/20220102074443_create_movies.js
+++ data/migrations/20220102074443_create_movies.js
@@ -4,12 +4,13 @@ exports.up = function(knex) {
         tbl.increments('movie_id');
         tbl.string('imdb_id');
         tbl.string('title')
-          .notNullable();
+        .notNullable();
         tbl.text('description');
         tbl.string('release_date');
         tbl.string('rating');
         tbl.boolean('is_good');
         tbl.string('image_url');
+        tbl.datetime('created_at');
     });
   };
   
